83b00ef2bc75d3c8a7d6f9bbfc1d99b0437d6954
oreste-abizera <oresteabizera11@gmail.com>
2021-06-17 21:24:07 +0200

    finishing 🎉

---
+++ README.md
@@ -0,0 +1,1 @@
+Next js package with typescript and tailwind css configured
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
+
+const IndexPage: NextPage = () => {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1 className="m-4">Hello Next.js 👋</h1>
+    </Layout>
+  );
+};
+
+export default IndexPage;
